a2578e1160af78e61a808623116c806562683323
Gary Mazzone Peters <gary.m.peters@vanderbilt.edu>
2014-11-08 13:55:34 -0600

    2nd Change
    This is to boa boa bala ablabalablbal

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from flask import render_template, redirect
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+	user = { 'nickname': 'Mazzone'}
+	return render_template("index.html",
+		title = 'Home',
+		user = user
+		)
+
+
+@app.route('/about')
+def about():
+	return render_template("about.html",
+		title = 'About')
