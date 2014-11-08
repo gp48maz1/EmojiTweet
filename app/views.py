a8e5aab79d2ff20ddcd2d78e4f27695777f184dc
Gary Mazzone Peters <gary.m.peters@vanderbilt.edu>
2014-11-08 15:01:39 -0600

    Change 4
    Added the various pages WHICH JEFF NEEDS TO GET ER DONE

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from flask import render_template, redirect
 from app import app
 
+user = { 'nickname': 'Mazzone'}
+
 @app.route('/')
 @app.route('/index')
 def index():
-	user = { 'nickname': 'Mazzone'}
 	return render_template("index.html",
 		title = 'Home',
 		user = user
@@ -13,4 +14,21 @@ def index():
 @app.route('/about')
 def about():
 	return render_template("about.html",
-		title = 'About')
+		title = 'About',
+		user = user
+		)
+
+@app.route('/profile')
+def profile():
+	return render_template("profile.html",
+		title = 'Profile',
+		user = user
+		)
+
+@app.route('/newsfeed')
+def newsfeed():
+	return render_template("newsfeed.html",
+		title = 'News Feed',
+		user = user
+		)
+
